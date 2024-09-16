90128e3500f4e8f5d6af02cc05412c73e6ffea48
wphkweerasinghe <hkalhara777@gmail.com>
2024-09-17 00:30:42 +0530

    updated the new LWC component.

---
+++ force-app/main/default/lwc/branchDetails/branchDetails.js
@@ -0,0 +1,33 @@
+import { LightningElement, track } from 'lwc';
+import getRepoBranches from '@salesforce/apex/GitHubService.getRepoBranches';
+
+export default class BranchDetails extends LightningElement {
+    @track repoName = '';
+    @track branches;
+    @track error = false;
+    @track errorMessage = '';
+
+    handleInputChange(event) {
+        this.repoName = event.target.value;
+    }
+
+    async fetchBranches() {
+        if (!this.repoName) {
+            this.error = true;
+            this.errorMessage = 'Repository name is required';
+            return;
+        }
+
+        this.error = false;
+        this.errorMessage = '';
+        this.branches = null;
+
+        try {
+            const result = await getRepoBranches({ repoName: this.repoName });
+            this.branches = result;
+        } catch (error) {
+            this.error = true;
+            this.errorMessage = 'Error fetching branches: ' + error.body.message;
+        }
+    }
+}
